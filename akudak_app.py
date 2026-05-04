4d73eeab9f2e36ef5b6f599b74171a40474a70e0
natwx521 <116364409+natwx521@users.noreply.github.com>
2026-05-04 14:38:26 +0300

    Update akudak_app.py

--- akudak_app.py
+++ akudak_app.py
@@ -125,7 +125,7 @@ def set_background(image_file):
 /* 🔥 BURAYI EKLEDİN */
 section[data-testid="stSidebar"] > div {{
     background-color: rgba(255, 255, 255, 0.05) !important;
-    backdrop-filter: blur(8px);
+    backdrop-filter: blur(2px);
 }}
         .block-container {{
             background-color: rgba(255,255,255,0.88);
